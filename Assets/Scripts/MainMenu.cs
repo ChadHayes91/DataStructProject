75f01dc3242eba2cdfe6ad4887b5443a4036b072
swzy <33035377+swzy@users.noreply.github.com>
2018-07-15 03:33:34 -0400

    Game playable with levels, UI, and database

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startLevel;
+    public string levelSelect;
+    public string level1Tag;
+    public string level2Tag;
+    public string level3Tag;
+
+    public void NewGame()
+    {
+        PlayerPrefs.SetInt(level1Tag, 1);
+        PlayerPrefs.SetInt(level2Tag, 0);
+        PlayerPrefs.SetInt(level3Tag, 0);
+        SceneManager.LoadScene(startLevel);
+        PlayerPrefs.SetInt("PlayerLevelSelectPosition", 0);
+    }
+
+    public void LevelSelect()
+    {
+        PlayerPrefs.SetInt(level1Tag, 1);
+        SceneManager.LoadScene(levelSelect);
+
+        if (!PlayerPrefs.HasKey("PlayerLevelSelectPosition"))
+        {
+            PlayerPrefs.SetInt("PlayerLevelSelectPosition", 0);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
